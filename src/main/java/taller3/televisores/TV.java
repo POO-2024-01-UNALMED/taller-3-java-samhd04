bc923ec73b8686c82c3976a759ec411c709a797d
samhd04 <shernandezdu@unal.edu.co>
2024-02-22 20:43:43 -0500

    Prueba seis

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -78,24 +78,32 @@ public class TV {
 	public void canalUp() {
 		if(estado == true && canal < 120) {
 			canal = canal + 1;
+		} else {
+			return;
 		}
 	}
 	
 	public void canalDown() {
 		if(estado == true && canal > 1) {
 			canal = canal - 1;
+		} else {
+			return;
 		}
 	}
 	
 	public void volumenUp() {
 		if(estado == true && volumen < 7) {
 			volumen = volumen + 1;
+		} else {
+			return;
 		}
 	}
 	
 	public void volumenDown() {
 		if(estado == true && volumen > 0) {
 			volumen = volumen - 1;
+		} else {
+			return;
 		}
 	}
 }
